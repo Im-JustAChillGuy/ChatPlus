11c563ae059c47904ff6d822adb9365babce5a53
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-05-26 15:53:08 +0530

    Update ChatHudMixin.java

--- src/main/java/com/JustAChillGuy/chatplus/client/mixin/ChatHudMixin.java
+++ src/main/java/com/JustAChillGuy/chatplus/client/mixin/ChatHudMixin.java
@@ -1,7 +1,7 @@
 package com.JustAChillGuy.chatplus.client.mixin;
 
 import net.minecraft.client.gui.components.ChatComponent;;
-import net.minecraft.text.Text;
+import net.minecraft.network.chat.Component;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
